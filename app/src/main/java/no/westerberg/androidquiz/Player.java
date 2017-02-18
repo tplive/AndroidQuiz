079abdcdcf17c3c9550ed5f1da9e204af1cbacdb
Thomas Qvidahl <tp@live.no>
2017-02-18 22:09:57 +0100

    Player class

---
+++ app/src/main/java/no/westerberg/androidquiz/Player.java
@@ -0,0 +1,22 @@
+package no.westerberg.androidquiz;
+
+/**
+ * Created by Thomas on 18.02.2017.
+ */
+
+public class Player {
+    private int score;
+
+
+    public Player() {
+        this.score = 0;
+    }
+
+    public void scorePoint() {
+        this.score++;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+}
