41a260e04afd0ef04cd21467d9da50e38e0ae441
Thomas Qvidahl <tp@live.no>
2017-02-27 18:57:36 +0100

    Koblet onClick metode til buttonNext. Finner hvilken radiobutton som er valgt ut fra RadioGroup'ens getCheckedRadioButtonId metode.

--- app/src/main/java/no/westerberg/androidquiz/QuestionActivity.java
+++ app/src/main/java/no/westerberg/androidquiz/QuestionActivity.java
@@ -3,9 +3,11 @@ package no.westerberg.androidquiz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class QuestionActivity extends AppCompatActivity {
@@ -52,8 +54,16 @@ public class QuestionActivity extends AppCompatActivity {
             Question[] questionBank = (Question[])i.getSerializableExtra("questionBank");
             displayNextQuestion(questionBank, questionIndex);
 
-
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioGroup group = (RadioGroup)findViewById(R.id.optionRadioGroup);
+                RadioButton playerAnswer = (RadioButton)findViewById(group.getCheckedRadioButtonId());
+            }
+        });
 
 
     }
+
+
 }
