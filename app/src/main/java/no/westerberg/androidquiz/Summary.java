e6fd56e33fd9b48a8ea735502e60a2130d4e1fc9
Thomas Qvidahl <tp@live.no>
2017-03-11 16:33:01 +0100

    Load saved state in Summary

--- app/src/main/java/no/westerberg/androidquiz/Summary.java
+++ app/src/main/java/no/westerberg/androidquiz/Summary.java
@@ -1,5 +1,6 @@
 package no.westerberg.androidquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class Summary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        if (savedInstanceState != null) {
+            questionIndex = savedInstanceState.getInt(QINDEX, 0);
+        }
+        Intent i = getIntent();
+        final Question[] questionBank = (Question[]) i.getSerializableExtra("questionBank");
+        final Player player1 = (Player) i.getSerializableExtra("player1");
     }
 }
