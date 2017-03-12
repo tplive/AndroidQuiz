6406d85fd7247e6e3aab77d25fdf57e1d6bc8def
Thomas Qvidahl <thomas.qvidahl@evry.com>
2017-03-12 14:22:50 +0100

    Rettet opp utregning slik at summen deles i fire kvartiler, og man får en tilbakemelding etter hvilken kvartil man er i.

--- app/src/main/java/no/westerberg/androidquiz/Summary.java
+++ app/src/main/java/no/westerberg/androidquiz/Summary.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Summary extends AppCompatActivity {
 
     private Player player1;
+    private Button resetButton;
     private static final String PLAYER1 = "player1";
     private static final String QINDEX = "qIndex";
     private static int questionIndex;
@@ -29,7 +31,27 @@ public class Summary extends AppCompatActivity {
 
         displayScore(player1.getScore(), questionIndex);
         displayDescription();
+
+        resetButton = (Button)findViewById(R.id.buttonReset);
+        resetButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Summary.this, MainActivity.class);
+                i.putExtra("player1", player1);
+                startActivity(i);
+
+                finish();
+
+            }
+        });
+
+
+
+
     }
+
+
 
     private void displayDescription() {
         TextView scoreDesc = (TextView) findViewById(R.id.textViewScoreDesc);
@@ -42,16 +64,14 @@ public class Summary extends AppCompatActivity {
 
         if (score == total) {
             return "Genialt! Alt rett! Android-superstjerne!";
-        }
-
-        if (total / score < quart * 3) {
-            return "Dette gikk ikke så bra, gjorde det vel?";
-        }else if (total / score <= quart * 2) {
-            return "Du må øve mer, du burde greie mer enn halvparten!";
-        }else if (total / score <= quart * 3) {
-            return "Se der ja, det gikk riktig så bra!";
-        }else {
+        }else if (score > quart * 3) {
             return "Helt i øvre skikt! Veldig bra jobba!";
+        }else if (score >= quart * 2) {
+            return "Se der ja, over middels!";
+        }else if (score >= quart ) {
+            return "Du må øve mer, du burde greie mer enn halvparten!";
+        }else {
+            return "Dette gikk ikke så bra, gjorde det vel?";
         }
 
     }
