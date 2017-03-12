df1a98ab3e2b98ece67175739319dcb0dd8d6765
Thomas Qvidahl <tp@live.no>
2017-03-12 23:06:44 +0100

    Overrider onSaveInstanceState i MainActivity for å ta vare på playerobjektet.

--- app/src/main/java/no/westerberg/androidquiz/MainActivity.java
+++ app/src/main/java/no/westerberg/androidquiz/MainActivity.java
@@ -3,6 +3,7 @@ package no.westerberg.androidquiz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,15 +20,22 @@ public class MainActivity extends AppCompatActivity {
             new Question("Hva er kodenavnet på Android 5.0?", "Cupcake", "Honeycomb", "Lollipop", "Nougat", 3),
             new Question("Hvem utviklet opprinnelig Android operativsystemet?", "Google", "Apple", "Microsoft", "Android Inc", 4),
             new Question("Hvilket år ble Jelly Bean lansert?", "2012", "2002", "2009", "2011", 1),
-            new Question("Hvem av disse var ikke med på grunnleggingen av Android Inc?", "Andy Rubin", "Chris White", "Rich Miner", "Eline Westerberg", 4)
+            new Question("Hvem av disse var ikke med på grunnleggingen av Android Inc?", "Andy Rubin", "Chris White", "Rich Miner", "Bill Gates", 4)
+            new Question("En av disse leverandørene bruker Android som sitt operativsystem. Hvilken?", "Apple", "Samsung", "Microsoft", "Nokia", 2)
 
     };
 
-
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putSerializable(PLAYER1, player1);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
         setContentView(R.layout.activity_main);
 
         onSaveInstanceState(savedInstanceState);
